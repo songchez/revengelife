8105a4755c07e658f59c185d95f04f3ae26846a3
songchez <tama4840@gmail.com>
2021-10-07 23:16:27 +0900

    ..

--- move.cs
+++ move.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,13 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     public int playerspeed = 0;
-    public Camera main_camera;
+    //public Camera main_camera;
     Animator animator;
     Vector3 position_maincharacter;
     Rigidbody2D rigid2d;
     Vector2 movement;
     string animationState = "MoveState";
+    bool fucker= true;
     enum States
     {
         right = 4,
@@ -24,20 +26,19 @@ public class move : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         rigid2d = GetComponent<Rigidbody2D>();
-        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Playermove();
-
     }
     void Update()
     {
         UpdateState();
-        position_maincharacter = new Vector3(transform.position.x, transform.position.y, -10);
-        main_camera.transform.position = position_maincharacter;
+        //position_maincharacter = new Vector3(transform.position.x, transform.position.y, -10);
+        //main_camera.transform.position = position_maincharacter;
+        
     }
 
     private void Playermove()
@@ -63,11 +64,6 @@ public class move : MonoBehaviour
             movement = new Vector2(0, playerspeed * Time.deltaTime);
             rigid2d.MovePosition(rigid2d.position + movement);
         }
-        Moving_event();
-    }
-
-    void Moving_event(){
-        
     }
     private void UpdateState()
     {
