1bbaea41c8dfb8cca488f107b15d28d5216fad51
songchez <tama4840@gmail.com>
2021-10-08 10:48:16 +0900

    tile_get

--- Controllers/TilespriteController.cs
+++ Controllers/TilespriteController.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Character;
+using Furniture;
+using Room;
 
 public class TilespriteController : MonoBehaviour
 {
+    public GameObject SampleTile;
+    public int Width;
+    public int Height;
+    public List<CharacterData> characters;
+    public List<FurnitureData> furnitures;
+    public List<RoomData> rooms;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetupTiles(Width,Height);
     }
 
-    // Update is called once per frame
-    void Update()
+    //건설타일 생성
+    void SetupTiles(int width, int height)
     {
-        
+        int x = 0;
+        int y = 0;
+        for (x = 0; x < width; x++)
+        {
+            for (y = 0; y < height; y++)
+            {
+                GameObject Tile = Instantiate(SampleTile, new Vector3(x, y), Quaternion.identity);
+                Tile.name = "타일_"+ x +","+ y;
+                Tile.transform.parent = this.transform;
+            }
+        }
     }
 }
